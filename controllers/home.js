1e8db55785d84d518d457b1c71a6201b3ceca1d9
Harry64C <harrisoncooper02@icloud.com>
2024-05-30 17:39:25 -0700

    set up the database and started on the homepage

--- controllers/home.js
+++ controllers/home.js
@@ -1,9 +1,58 @@
 // Controller handler to handle functionality in home page
 
-// Example for handle a get request at '/' endpoint.
+async function loadRooms() {
+
+  let roomsList = []
+  try {
+    let response = await fetch("http://localhost:8080/rooms");
+    let data = await response.text();
+
+    console.log(JSON.parse(data));
+    roomsList = JSON.parse(data);
+  } catch(error) {
+    console.log(error)
+  }
+
+
+//   if (response.status === 200) {
+//       let data = await response.text();
+//       console.log(data);
+//       const books = JSON.parse(data);
+
+//       for (let book of books) {
+//           const x = `
+// <div class = "col-4">
+//   <div class = "card">
+//       <div class = "card-body">
+//           <h5 class = "card-title">${book.title}</h5>
+//           <h6 class = "card-subtitle mb-2 text-muted">${book.isbn}</h6>
+
+//           <div>Author: ${book.author}</div>
+//           <div>Publisher: ${book.publisher}</div>
+//           <div>Number of Pages: ${book.numOfPages}</div>
+
+//           <hr>
+
+//           <button type = "button" onClick = "deleteBook(${book.isbn})" class = "btn btn-danger">Delete</button>
+
+//           <button types = "button" class = "btn btn-primary" data-toggle = "modal"
+//               data-target = "#editBookModal" onClick = "setEditModal(${book.isbn})">
+//               Edit
+//           </button>
+//       </div>
+//   </div>
+// </div>      
+//           `
+
+//           document.getElementById('rooms').innerHTML += x;
+//       }
+//   }
+}
+
 
 function getHome(request, response){
-  // do any work you need to do, then
+  
+  loadRooms();
   response.render('home', {title: 'home'});
 }
 
--- server.js
+++ server.js
@@ -4,6 +4,21 @@ const cookieParser = require('cookie-parser');
 const hbs = require('express-handlebars');
 const path = require('path');
 
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const bodyParser = require('body-parser')
+
+
+  async function run() {
+    const client = new MongoClient(process.env.DATABASE_URL);
+    await client.connect()
+    .then(console.log("connected to database"))
+
+
+    await client.close();
+  }
+  run();
+
 // import handlers
 const homeHandler = require('./controllers/home.js');
 const roomHandler = require('./controllers/room.js');
@@ -28,8 +43,23 @@ app.set('view engine', 'hbs');
 
 // Create controller handlers to handle requests at each endpoint
 app.get('/', homeHandler.getHome);
+
+// create route for the room information
+app.get('/rooms', (request, response) => {
+    const roomData = [
+        { roomName : "room1" },
+        { roomName : "room2" },
+    ]
+
+    response.json(roomData);
+});
+
 app.get('/:roomName', roomHandler.getRoom);
 
-// NOTE: This is the sample server.js code we provided, feel free to change the structures
+
+const router = express.Router();
+
+//Export the router
+module.exports = router;
 
 app.listen(port, () => console.log(`Server listening on http://localhost:${port}`));
