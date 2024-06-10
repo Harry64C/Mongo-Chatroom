3309255301b71e0bcd1c74874a9d6aadede10375
Niikkc <nkang1230@gmail.com>
2024-06-10 02:08:11 -0700

    Sanitized the login and register

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const session = require('express-session');
 require("dotenv").config();
 const { MongoClient, ObjectId } = require('mongodb');
 const bodyParser = require('body-parser');
+const { body, validationResult, check } = require('express-validator');
 
 const homeHandler = require('./controllers/home.js');
 const roomHandler = require('./controllers/room.js');
@@ -93,33 +94,98 @@ app.get('/login', (req, res) => {
     res.render('login');
 });
 
-app.post('/login', async (req, res) => {
-    const { username, password } = req.body;
-    const user = await getUserFromDatabase(username);
+app.post('/login', 
+    [
+        check('username').isLength({ min: 1 }).trim().escape(),
+        check('password').isLength({ min: 1 }).trim().escape()
+    ],
+    async (req, res) => {
 
-    if (user && await bcrypt.compare(password, user.password)) {
-        req.session.user = user;
-        res.redirect('/');
-    } else {
-        res.render('login', { error: 'Invalid username or password' });
-    }
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        const { username, password } = req.body;
+
+        try{
+            const user = await getUserFromDatabase(username);
+
+            if (user && await bcrypt.compare(password, user.password)) {
+
+                req.session.user = user;
+                res.redirect('/');
+
+            } else {
+
+                res.render('login', { error: 'Invalid username or password' });
+
+            }
+        }catch (e) {
+            console.error(e);
+            res.sendStatus(500);
+        }
 });
+
+/*app.post('/message',
+    [
+        check('message').isLength({ min: 1 }).trim().escape(),
+    ], 
+    async (request, response) => { 
+
+        const errors = validationResult(request);
+
+        if (!errors.isEmpty()) {
+            return response.status(400).json({errors: errors.array()});
+        }
+
+        try {
+
+            await insertInDB('chats', request.body);
+            console.log('Message has been posted');
+            response.redirect('back');
+
+        } catch (error) {
+
+            console.log(error);
+            response.sendStatus(500);
+
+        }
+  });
+*/
 
 app.get('/register', (req, res) => {
     res.render('register');
 });
 
-app.post('/register', async (req, res) => {
-    const { username, password } = req.body;
-    const user = await getUserFromDatabase(username);
+app.post('/register', 
+    [
+        check('username').isLength({ min: 1 }).trim().escape(),
+        check('password').isLength({ min: 1 }).trim().escape()
+    ],
+    async (req, res) => {
 
-    if (user) {
-        res.redirect('/register');
-    } else {
-        await insertUser(username, password);
-        res.redirect('/login');
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        const { username, password } = req.body;
+        try{
+            const user = await getUserFromDatabase(username);
+
+            if (user) {
+                res.redirect('/register');
+            } else {
+                await insertUser(username, password);
+                res.redirect('/login');
+            }
+        }catch (e) {
+            console.error(e);
+            res.sendStatus(500);
+        }
     }
-});
+);
 
 app.get('/', isAuthenticated, homeHandler.getHome);
 
